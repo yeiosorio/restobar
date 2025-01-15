fe278370d06d93b27585c14ce8fd70b1011cb5ca
yeiosorio <yei_osorio@hotmail.com>
2025-01-15 09:48:30 -0500

    feat: (deploy firebase) se configura urls para apuntar al server del API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://restobar-33413.web.app/api',
+  apiUrl: 'https://restobar-api.onrender.com',
   firebase: {
-    apiKey: "AIzaSyA4yB5w0aqgNh6nF61trvDxwkqSYaEknIw",
+    apiKey: "tu-api-key",
     authDomain: "restobar-33413.firebaseapp.com",
     projectId: "restobar-33413",
-    storageBucket: "restobar-33413.firebasestorage.app",
-    messagingSenderId: "582740793714",
-    appId: "1:582740793714:web:886f278dd028cfa305a5eb"
+    storageBucket: "restobar-33413.appspot.com",
+    messagingSenderId: "tu-messaging-sender-id",
+    appId: "tu-app-id"
   }
 };
